24b08c51b6a0a4f45fb7ee4077a3683cf95fb225
ekaterina-chernykh <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-04 14:47:48 -0400

    js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Ekaterina All rights reserved
+//
+// Created by: Ekaterina
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+ function myButtonClicked() {
+    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  }
